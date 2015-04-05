d8f96a1794931d53cc85cff76ca1d7ac5a536cd2
Boni Garcia <bgarcia@gsyc.es>
2015-04-05 21:48:43 +0200

    Added spring-data-mongodb example

---
+++ spring-data-mongodb/src/main/java/com/utad/web/data/mongodb/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.utad.web.data.mongodb;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, String> {
+
+	List<Customer> findByLastName(String lastName);
+
+	List<Customer> findByFirstName(String firstName);
+
+}
