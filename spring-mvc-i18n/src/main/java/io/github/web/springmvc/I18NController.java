280df1462557cc4013e1442b714d141a2631a046
Boni Garcia <bgarcia@gsyc.es>
2015-10-22 09:54:34 +0200

    Added spring-mvc-i18n example

---
+++ spring-mvc-i18n/src/main/java/io/github/web/springmvc/I18NController.java
@@ -0,0 +1,15 @@
+package io.github.web.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class I18NController {
+
+	@RequestMapping("/i18n")
+	public ModelAndView i18n() {
+		return new ModelAndView("i18n_page");
+	}
+
+}
---
+++ spring-mvc-i18n/src/main/java/io/github/web/springmvc/SpringMvcI18nApp.java
@@ -0,0 +1,48 @@
+package io.github.web.springmvc;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+public class SpringMvcI18nApp extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor result = new LocaleChangeInterceptor();
+		result.setParamName("lang");
+		return result;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcI18nApp.class, args);
+	}
+}
