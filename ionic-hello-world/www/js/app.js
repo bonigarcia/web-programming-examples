dc8b6338edb2b10fa89a7ce8d03cf85ff1340c1b
Boni Garcia <bgarcia@gsyc.es>
2016-05-03 05:22:52 +0200

    Modify myApp.run in ionic-hello-world app

--- ionic-hello-world/www/js/app.js
+++ ionic-hello-world/www/js/app.js
@@ -1,4 +1,12 @@
 var myApp = angular.module('myApp', [ 'ionic' ]);
 
 myApp.run(function($ionicPlatform) {
-});
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
