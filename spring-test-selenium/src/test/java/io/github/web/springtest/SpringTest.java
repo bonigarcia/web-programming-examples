c4fa24446a95f2ddad845966c9c58fb131889105
Boni Garcia <bgarcia@gsyc.es>
2017-03-27 17:57:56 +0200

    Update test-selenium project

--- spring-test-selenium/src/test/java/io/github/web/springtest/SpringTest.java
+++ spring-test-selenium/src/test/java/io/github/web/springtest/SpringTest.java
@@ -1,7 +1,5 @@
 package io.github.web.springtest;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
@@ -14,54 +12,56 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@WebIntegrationTest
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringTestDemoApp.class)
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringTestDemoApp.class)
+@WebAppConfiguration
 public class SpringTest {
 
-    private static final long TIMEOUT = 30; // seconds
-    private WebDriver driver;
+	private static final long TIMEOUT = 30; // seconds
+	private WebDriver driver;
 
-    @BeforeClass
-    public static void setupClass() {
-        ChromeDriverManager.getInstance().setup();
-    }
+	@BeforeClass
+	public static void setupClass() {
+		ChromeDriverManager.getInstance().setup();
+	}
 
-    @Before
-    public void setupTest() {
-        driver = new ChromeDriver();
-    }
+	@Before
+	public void setupTest() {
+		driver = new ChromeDriver();
+	}
 
-    @After
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+	@After
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 
-    @Test
-    public void test() {
-        // Always wait TIMEOUT seconds
-        driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
+	@Test
+	public void test() {
+		// Always wait TIMEOUT seconds
+		driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
 
-        // Open system under test
-        driver.get("http://localhost:8080/");
+		// Open system under test
+		driver.get("http://localhost:8080/");
 
-        // Verify first page title
-        Assert.assertTrue(ExpectedConditions
-                .titleIs("Spring Boot Test - Page 1").apply(driver));
+		// Verify first page title
+		Assert.assertTrue(ExpectedConditions
+				.titleIs("Spring Boot Test - Page 1").apply(driver));
 
-        // Click on link
-        driver.findElement(By.linkText("another")).click();
+		// Click on link
+		driver.findElement(By.linkText("another")).click();
 
-        // Verify second page text content
-        Assert.assertTrue(ExpectedConditions
-                .textToBePresentInElementLocated(By.tagName("body"), "Hello")
-                .apply(driver));
-    }
+		// Verify second page text content
+		Assert.assertTrue(ExpectedConditions
+				.textToBePresentInElementLocated(By.tagName("body"), "Hello")
+				.apply(driver));
+	}
 
 }
