6b054dd047d770fafc2f95ddad05c802f9552629
Boni Garcia <bgarcia@gsyc.es>
2017-04-24 18:11:41 +0200

    Include Angular data binding example

---
+++ angular-data-binding/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+    name = 'Anybody';
+    imgUrl = "assets/utad.png";
+
+    setName(name: string) {
+        this.name = name;
+    }
+    
+    changeUrl() {
+        this.imgUrl = "assets/angular.png";
+    }
+}
