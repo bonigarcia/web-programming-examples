af66f3d3bdc885c96067a04ace5de0004491e419
Boni Garcia <bgarcia@gsyc.es>
2017-05-20 22:05:19 +0200

    Include REST service for films (API like OMDb)

---
+++ film-rest-service/src/main/java/io/github/web/rest/service/FilmsServiceApp.java
@@ -0,0 +1,12 @@
+package io.github.web.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmsServiceApp.class, args);
+    }
+}
