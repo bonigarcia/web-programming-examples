2ce0421c7da1a640c88b10d1708c127499a379b0
Boni Garcia <bgarcia@gsyc.es>
2018-01-01 12:16:11 +0100

    Update spring-test-selenium project

--- spring-test-selenium/src/test/java/io/github/web/springtest/SpringChromeTest.java
+++ spring-test-selenium/src/test/java/io/github/web/springtest/SpringChromeTest.java
@@ -11,8 +11,8 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
@@ -21,49 +21,49 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 @SpringBootTest(classes = SpringTestDemoApp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringChromeTest {
 
-	private WebDriver driver;
+    private WebDriver driver;
 
-	@LocalServerPort
-	protected int serverPort;
+    @LocalServerPort
+    protected int serverPort;
 
-	@BeforeClass
-	public static void setupClass() {
-		ChromeDriverManager.getInstance().setup();
-	}
+    @BeforeClass
+    public static void setupClass() {
+        ChromeDriverManager.getInstance().setup();
+    }
 
-	@Before
-	public void setupTest() {
-		driver = new ChromeDriver();
-	}
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
 
-	@After
-	public void teardown() {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
-	@Test
-	public void test() {
-		// Always wait 30 seconds to locate elements
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    @Test
+    public void test() {
+        // Always wait 30 seconds to locate elements
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-		// Open system under test
-		driver.get("http://localhost:" + serverPort);
+        // Open system under test
+        driver.get("http://localhost:" + serverPort);
 
-		// Verify first page title
-		String firstPageTitle = driver.getTitle();
-		String expectedFirstPageTitle = "Spring Boot Test - Page 1";
-		Assert.assertEquals(expectedFirstPageTitle, firstPageTitle);
+        // Verify first page title
+        String firstPageTitle = driver.getTitle();
+        String expectedFirstPageTitle = "Spring Boot Test - Page 1";
+        Assert.assertEquals(expectedFirstPageTitle, firstPageTitle);
 
-		// Click on link
-		driver.findElement(By.linkText("another")).click();
+        // Click on link
+        driver.findElement(By.linkText("another")).click();
 
-		// Verify second page caption
-		String secondPageCaption = driver.findElement(By.id("caption"))
-				.getText();
-		String expectedSecondPageTitle = "Other page";
-		Assert.assertEquals(expectedSecondPageTitle, secondPageCaption);
-	}
+        // Verify second page caption
+        String secondPageCaption = driver.findElement(By.id("caption"))
+                .getText();
+        String expectedSecondPageTitle = "Other page";
+        Assert.assertEquals(expectedSecondPageTitle, secondPageCaption);
+    }
 
 }
