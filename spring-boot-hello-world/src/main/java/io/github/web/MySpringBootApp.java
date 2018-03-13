a27bb966aa8d2cce2f89dbf663d29689b86024c8
Boni Garcia <bgarcia@gsyc.es>
2018-03-13 21:49:39 +0100

    Update spring-boot-hello-world to spring-boot 2.0.0

--- spring-boot-hello-world/src/main/java/io/github/web/MySpringBootApp.java
+++ spring-boot-hello-world/src/main/java/io/github/web/MySpringBootApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MySpringBootApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MySpringBootApp.class, args);
     }
 }
