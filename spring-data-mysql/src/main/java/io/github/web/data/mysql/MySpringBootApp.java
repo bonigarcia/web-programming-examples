451688f4dfe3bd571009c485c5fcd65213b4a130
Boni Garcia <bgarcia@gsyc.es>
2018-04-01 12:51:11 +0200

    Update spring-data-mysql project

--- spring-data-mysql/src/main/java/io/github/web/data/mysql/MySpringBootApp.java
+++ spring-data-mysql/src/main/java/io/github/web/data/mysql/MySpringBootApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MySqlApplication {
+public class MySpringBootApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(MySqlApplication.class, args);
+        SpringApplication.run(MySpringBootApp.class, args);
     }
 }
