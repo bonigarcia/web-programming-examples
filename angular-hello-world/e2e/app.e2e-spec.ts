7d096337b7ee990682e8bb7be2a061020c6d476c
Boni Garcia <bgarcia@gsyc.es>
2018-04-19 01:07:11 +0200

    Update angular-hello-world to Angular 5

--- angular-hello-world/e2e/app.e2e-spec.ts
+++ angular-hello-world/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularHelloWorldPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('angular-hello-world App', () => {
-  let page: AngularHelloWorldPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularHelloWorldPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
