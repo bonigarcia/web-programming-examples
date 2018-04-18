de28119a05efc27a268be50ccd4dfc12637410d5
Boni Garcia <bgarcia@gsyc.es>
2018-04-19 01:36:20 +0200

    Update heroku-spring-boot-postgresql project

--- heroku-spring-boot-postgresql/src/main/java/io/github/web/heroku/MySpringBootApp.java
+++ heroku-spring-boot-postgresql/src/main/java/io/github/web/heroku/MySpringBootApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HerokuApplication {
+public class MySpringBootApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(HerokuApplication.class, args);
+        SpringApplication.run(MySpringBootApp.class, args);
     }
 }
