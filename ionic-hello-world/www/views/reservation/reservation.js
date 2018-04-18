24b8d70f588dbcac9e12b580944b57bfd6594dc9
Boni Garcia <bgarcia@gsyc.es>
2018-04-19 00:14:06 +0200

    Rename ionic-hello-world project

--- ionic-hello-world/www/views/reservation/reservation.js
+++
@@ -1,9 +0,0 @@
-myApp.controller('ReservationController', function($scope) {
-    $scope.reservation = {
-        checkin : new Date(),
-        checkout : new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
-        room : 156,
-        rate : 121,
-        wifi : 'resortwifi'
-    };
-});
