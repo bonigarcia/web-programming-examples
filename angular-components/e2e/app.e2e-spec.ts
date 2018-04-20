dabb330050e486e96c914f26111b2a5690465cea
Boni Garcia <bgarcia@gsyc.es>
2018-04-20 18:35:01 +0200

    Update angular-components project

--- angular-components/e2e/app.e2e-spec.ts
+++ angular-components/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularComponentsPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('angular-components App', () => {
-  let page: AngularComponentsPage;
+describe('angular-hello-world App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularComponentsPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- angular-components/e2e/app.po.ts
+++ angular-components/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class AngularComponentsPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
