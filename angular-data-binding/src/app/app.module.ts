15615077f79b87fe599f11cec18a67f1340ab5a5
Boni Garcia <bgarcia@gsyc.es>
2018-04-22 13:14:37 +0200

    Remove non-necessary carriage returns in app.module.ts

--- angular-data-binding/src/app/app.module.ts
+++ angular-data-binding/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
-
 import {AppComponent} from './app.component';
-
 
 @NgModule({
   declarations: [
