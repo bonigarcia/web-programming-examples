7f8c17f16b6ca19469bfeb7e1086671ee26b6c7a
Boni Garcia <bgarcia@gsyc.es>
2018-04-22 18:43:57 +0200

    Add angular-bootstrap project

---
+++ angular-bootstrap/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Angular App with Bootstrap style</h1>
+    <button type="button" class="btn btn-primary">Primary</button>   
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+}
