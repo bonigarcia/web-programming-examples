a4a05d17ac2bcbd7604cc1994ddc493982df9c3b
Boni Garcia <bgarcia@gsyc.es>
2018-04-22 19:16:34 +0200

    Add angular-material project

---
+++ angular-material/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styles: []
+})
+export class AppComponent {
+}
---
+++ angular-material/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+
+import {AppComponent} from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
