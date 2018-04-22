fdefeb62799fb1d6480d46298e39adb740c0e38e
Boni Garcia <bgarcia@gsyc.es>
2018-04-22 14:12:15 +0200

    Remove search logic from the controller in angular-rest-services project

--- angular-rest-services/src/app/app.component.ts
+++ angular-rest-services/src/app/app.component.ts
@@ -12,19 +12,6 @@ export class AppComponent {
 
     constructor(private http: Http, private service: BooksService) { }
 
-    seek(title: string) {
-        this.books = [];
-        let url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title;
-        this.http.get(url).subscribe(
-            response => {
-                for (let book of response.json().items) {
-                    this.books.push(book.volumeInfo.title);
-                }
-            },
-            error => console.error(error)
-        );
-    }
-
     search(title: string) {
         this.books = [];
         this.service.getBooks(title).subscribe(
